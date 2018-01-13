9a2c8837150962f1e5d917558557cfc1cf9ec957
Luv <lkapur@myseneca.ca>
2018-01-12 20:13:26 -0500

    added keyup event listener

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,23 @@
 extern crate ggez;
 use ggez::*;
 use ggez::graphics::{DrawMode, Point2};
+use ggez::event::{Mod, Keycode};
 
 struct MainState {
     pos_x: f32,
+    pos_y:f32
 }
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+        let s = MainState { pos_x: 0.0, pos_y: 380.0 };
         Ok(s)
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+
         self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
@@ -24,17 +27,36 @@ impl event::EventHandler for MainState {
         graphics::clear(ctx);
         graphics::circle(ctx,
                          DrawMode::Fill,
-                         Point2::new(self.pos_x, 380.0),
+                         Point2::new(self.pos_x, self.pos_y),
                          100.0,
                          2.0)?;
         graphics::present(ctx);
         Ok(())
     }
+
+    fn key_down_event(
+        &mut self, 
+        ctx: &mut Context, 
+        keycode: Keycode, 
+        _keymod: Mod, 
+        _repeat: bool
+    ) { 
+        self.pos_y = self.pos_y - 10.0;
+    }
+
+    fn key_up_event(
+        &mut self, 
+        _ctx: &mut Context,
+        _keycode: Keycode,
+        _keymod: Mod,
+        _repeat: bool) {
+        self.pos_x = self.pos_x + 10.00;
+    }
 }
 
 pub fn main() {
     let c = conf::Conf::new();
-    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
+    let ctx = &mut Context::load_from_conf("jailai", "pl", c).unwrap();
     let state = &mut MainState::new(ctx).unwrap();
     event::run(ctx, state).unwrap();
 }
